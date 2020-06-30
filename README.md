ebb3e4e51a3749cd578ead37ef2b4b47099910ac
Ken Thomas <ken.tms@gmail.com>
2020-06-30 14:27:37 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<p align="center">
+<a href="https://dialoguewise.com" target="_blank"><img src="https://github.com/dialoguewise/dialoguewise-demo/blob/master/screenshots/logo.PNG?raw=true"></a>
+</p>  
+<br/>
+<p align="center">
+Headless CMS to Manage Multi-Platform Content
+</p>  
+<br/>
+
+## About this repo?
+
+This is a demo app to show how you can integrate Dialogue Wise into your [Vue.js](https://vuejs.org/) apps.
+
+To install the package: 
+
+```
+npm i dialogue-wise
+```
+
+To run this repo, first run
+```
+yarn install
+```
+
+and then
+
+```
+yarn serve
+```
+
+to launch the application.
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div v-html="heroContent"></div>
+  </div>
+</template>
+
+<script>
+import { DialogueWiseService } from 'dialogue-wise';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      heroContent: '' 
+    }
+  },
+  mounted: async function () {
+    let request = {
+    slug: 'hero-section',
+    apiKey: 'b1266377591c4f2a9494c3abdd2cac5381D6Z825D26CEBAE8B6rn',
+    emailHash: '/kgmM46s1xC56BOFWRZp4j+0bdU19URpXdNT9liAX50=',
+  };
+
+  let dialogueWiseService = new DialogueWiseService();
+  let res = await dialogueWiseService.getDialogue(request);
+  this.heroContent = res['dialogue'][0]['hero-content'];
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
